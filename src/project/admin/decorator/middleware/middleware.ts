27dc8fbe993d82265706ea142b2a68582cb42121
lst <lidalanlah5@163.com>
2020-08-28 18:20:41 +0800

    m

--- src/project/admin/decorator/middleware/middleware.ts
+++ src/project/admin/decorator/middleware/middleware.ts
@@ -1,4 +1,6 @@
-export async function Log(ctx: any, next: any) {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export async function Log(ctx: any, next: any): Promise<void> {
 	console.log(123);
 	await next();
 }
